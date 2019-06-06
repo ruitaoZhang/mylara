8229910db1174411594d65bac51169a47e187b03
zrt <1042252577@qq.com>
2019-06-06 18:27:40 +0800

    guzzle

--- app/Tools/MyGuzzle.php
+++ app/Tools/MyGuzzle.php
@@ -9,7 +9,8 @@
 class MyGuzzle
 {
 
-    public function get(){
+    public static  function get()
+    {
         $client = new \GuzzleHttp\Client();
 
         $url = 'api-chr.com/admin/users/nationalities';
@@ -22,6 +23,82 @@ class MyGuzzle
         ];
         $response = $client->request('GET', $url, $array);
         dump($response->getStatusCode());   #打印响应信息
+        dump(json_decode($response->getBody()->getContents()));   #输出结果
+    }
+
+    static public function post()
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $url = 'api-chr.com/admin/users/nationalities';
+        $array = [
+            'json' => [
+                'name'=>'意大利',
+                'en_name'=>'Italy',
+                'kyc_type'=>1,
+                'order'=>0,
+                'is_forbid'=>0,
+            ],
+            'query' => [],
+            'http_errors' => false
+        ];
+        $response = $client->request('post', $url, $array);
+        dump($response->getStatusCode());   #打印响应信息
+        dump(json_decode($response->getBody()->getContents()));   #输出结果
+    }
+
+    public static  function put()
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $url = 'api-chr.com/admin/users/nationalities/6';
+        $array = [
+            'json' => [
+                'name'=>'意大利1',
+                'en_name'=>'Italy1',
+                'kyc_type'=>1,
+                'order'=>0,
+                'is_forbid'=>0,
+            ],
+            'query' => [],
+            'http_errors' => false
+        ];
+        $response = $client->request('put', $url, $array);
+        dump($response->getStatusCode());   #打印响应信息
+        dump(json_decode($response->getBody()->getContents()));   #输出结果
+    }
+
+    public static  function delete()
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $url = 'api-chr.com/admin/users/nationalities/6';
+        $array = [
+            'json' => [],
+            'query' => [],
+            'http_errors' => false
+        ];
+        $response = $client->request('delete', $url, $array);
+        dump($response->getStatusCode());   #打印响应信息
+        dump(json_decode($response->getBody()->getContents()));   #输出结果
+    }
+
+    public static  function upload()
+    {
+        $client = new \GuzzleHttp\Client();
+        $body = fopen('/home/summer/图片/dog1.jpg', 'r');
+        $response = $client->request('POST', 'http://account.chr.test/upload',
+            [
+                'multipart' => [
+                    [
+                        'name' => 'body',
+                        'contents' => fopen('/home/summer/图片/dog1.jpg', 'r')
+                    ],
+                ]
+            ]
+        );
+        dump($response->getStatusCode());   #打印响应信息
+        dump($response->getBody());
         dump(json_decode($response->getBody()->getContents()));   #输出结果
     }
 }
